38c240536d457ccd075ee0cf59c6c4b48d86c245
pharaox <pharaox@tutanota.com>
2024-04-07 16:12:12 +0300

    Adapt to Active Courtiers 0.4.0

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -4,6 +4,8 @@
 echo "accou_marriage.0001 event option a: $(grep -a "accou_marriage.0001 event option a" debug.log | wc -l)"
 echo "- success: $(grep -a "accou_marriage.0001 event option a success" debug.log | wc -l)"
 echo "- failure: $(grep -a "accou_marriage.0001 event option a failure" debug.log | wc -l)"
+#echo "  - no courtier: $(pcre2grep -a "accou_marriage.0001 event option a failure, courtier:  \(4294967295\)" debug.log | wc -l)"
+#echo "  - no candidate: $(pcre2grep -a "accou_marriage.0001 event option a failure, courtier: .+ \([\d]+\), candidate:  \(4294967295\)" debug.log | wc -l)"
 echo "- failure fp: $(pcre2grep -a "accou_marriage.0001 event option a failure, courtier: .+ \([\d]+\), candidate: .+ \([\d]+\), matchmaker: .+ \([\d]+\)" debug.log | wc -l)"
 echo
 echo "accou_marriage.0002 event option a: $(grep -a "accou_marriage.0002 event option a" debug.log | wc -l)"
@@ -12,8 +14,23 @@ echo "- insider: $(grep -a "accou_marriage.0002 event option a, .*, outsider: no
 echo "accou_marriage.0002 event option b: $(grep -a "accou_marriage.0002 event option b" debug.log | wc -l)"
 echo "- outsider: $(grep -a "accou_marriage.0002 event option b, .*, outsider: yes" debug.log | wc -l)"
 echo "- insider: $(grep -a "accou_marriage.0002 event option b, .*, outsider: no" debug.log | wc -l)"
-pcre2grep -a -o1 "candidate quality: ([\d]+)" debug.log | awk '{s+=$1}END{print "average candidate quality:",s/NR}' RS="\n"
+pcre2grep -a -o1 "accou_marriage.0002 event .* candidate quality: ([\d]+)" debug.log | awk '{s+=$1}END{print "average marriage candidate quality:",s/NR}' RS="\n"
 echo
+
+# Invite events
+echo "accou_invite.0001 event option a: $(grep -a "accou_invite.0001 event option a" debug.log | wc -l)"
+echo "- success: $(grep -a "accou_invite.0001 event option a success" debug.log | wc -l)"
+echo "- failure: $(grep -a "accou_invite.0001 event option a failure" debug.log | wc -l)"
+#echo "  - no candidate: $(pcre2grep -a "accou_invite.0001 event option a failure, candidate:  \(4294967295\)" debug.log | wc -l)"
+#echo "  - no courtier: $(pcre2grep -a "invite.0001 event option a failure, candidate: .+ \([\d]+\), courtier:  \(4294967295\)" debug.log | wc -l)"
+echo "- failure fp: $(pcre2grep -a "accou_invite.0001 event option a failure, candidate: .+ \([\d]+\), courtier: .+ \([\d]+\)" debug.log | wc -l)"
+echo
+echo "accou_invite.0002 event option a: $(grep -a "accou_invite.0002 event option a" debug.log | wc -l)"
+echo "accou_invite.0002 event option b: $(grep -a "accou_invite.0002 event option b" debug.log | wc -l)"
+pcre2grep -a -o1 "accou_invite.0002 event .* candidate quality: ([\d]+)" debug.log | awk '{s+=$1}END{print "average invite candidate quality:",s/NR}' RS="\n"
+echo
+
+# Courtier events
 echo "accou_courtier.0001 event option a: $(grep -a "accou_courtier.0001 event option a" debug.log | wc -l)"
 echo "- success: $(grep -a "accou_courtier.0001 event option a success" debug.log | wc -l)"
 echo "- failure: $(grep -a "accou_courtier.0001 event option a failure" debug.log | wc -l)"
@@ -72,3 +89,29 @@ echo "- court_smith: $(grep -a "accou_courtier.0003 event option a, .*, position
 echo "---"
 echo "accou_courtier.0003 event option b: $(grep -a "accou_courtier.0003 event option b" debug.log | wc -l)"
 echo
+
+# Match rates
+# echo "quick_marriage_ai_accept_modifier: $(grep -a "quick_marriage_ai_accept_modifier" debug.log | wc -l)"
+# echo "- ok: $(grep -a "quick_marriage_ai_accept_modifier ok" debug.log | wc -l)"
+# echo "- false positive: $(grep -a "quick_marriage_ai_accept_modifier false positive" debug.log | wc -l)"
+# echo "- false negative: $(grep -a "quick_marriage_ai_accept_modifier false negative" debug.log | wc -l)"
+# echo
+# echo "quick_invite_ai_accept_modifier: $(grep -a "quick_invite_ai_accept_modifier" debug.log | wc -l)"
+# echo "- ok: $(grep -a "quick_invite_ai_accept_modifier ok" debug.log | wc -l)"
+# echo "- false positive: $(grep -a "quick_invite_ai_accept_modifier false positive" debug.log | wc -l)"
+# echo "- false negative: $(grep -a "quick_invite_ai_accept_modifier false negative" debug.log | wc -l)"
+# echo
+
+# echo "Statistics"
+# echo "Year,Rul,Cou,Adu,Mar,SoS,GCV,Ch,ChS,St,StS,Ma,MaS,Sp,SpS,CC,CCS,CP,CPA,An,AnA,TL,TLA,CT,CTA,Mh,MhA,Se,SeA,Kn,KnS"
+# pcre2grep -a -o1 -o2 --om-separator=',' "([\d]+): Statistics \(all\): ([\d\.\,\-]+)" debug.log
+# echo "Counts"
+# echo "Year,Rul,Cou,Adu,Mar,SoS,GCV,Ch,ChS,St,StS,Ma,MaS,Sp,SpS,CC,CCS,CP,CPA,An,AnA,TL,TLA,CT,CTA,Mh,MhA,Se,SeA,Kn,KnS"
+# pcre2grep -a -o1 -o2 --om-separator=',' "([\d]+): Statistics \(counts\): ([\d\.\,\-]+)" debug.log
+# echo "Dukes"
+# echo "Year,Rul,Cou,Adu,Mar,SoS,GCV,Ch,ChS,St,StS,Ma,MaS,Sp,SpS,CC,CCS,CP,CPA,An,AnA,TL,TLA,CT,CTA,Mh,MhA,Se,SeA,Kn,KnS"
+# pcre2grep -a -o1 -o2 --om-separator=',' "([\d]+): Statistics \(dukes\): ([\d\.\,\-]+)" debug.log
+# echo "Kings"
+# echo "Year,Rul,Cou,Adu,Mar,SoS,GCV,Ch,ChS,St,StS,Ma,MaS,Sp,SpS,CC,CCS,CP,CPA,An,AnA,TL,TLA,CT,CTA,Mh,MhA,Se,SeA,Kn,KnS"
+# pcre2grep -a -o1 -o2 --om-separator=',' "([\d]+): Statistics \(kings\): ([\d\.\,\-]+)" debug.log
+# echo
